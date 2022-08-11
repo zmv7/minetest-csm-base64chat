04584b7bf468d7b4d0b1abd3cb7aa023ee09ca98
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-08-11 18:41:04 +0500

    Big fix

--- init.lua
+++ init.lua
@@ -1,22 +1,18 @@
-minetest.register_on_receiving_chat_message(function(message)
-local text
-local sender = message:match('%S+[:?>?] ')
-local csender = message:match('%S+[:?>?] %S+%(%S+%)')
-if csender then
-local data = message:gsub('[%a*%d*]+[:?>?] %S+%(?%S+%)','')
-text = core.decode_base64(data)
-elseif sender then
-local data = message:gsub('[%a*%d*]+[:?>?] ','')
-text = core.decode_base64(data)
-end
-if text then
-minetest.after(0.1,function()
-   core.display_chat_message(minetest.colorize('#FF0','Base64: '..sender)..text)
+core.register_on_receiving_chat_message(function(message)
+    message = core.strip_colors(message)
+    local text
+    local sender, data = message:match("(%S+[:?>?])%s?(.+)")
+    if data then
+        text = core.decode_base64(data)
+    end
+    if text and sender then
+        core.after(0.1,function()
+            core.display_chat_message(core.colorize('#FF0','Base64: '..sender).." "..text)
+        end)
+    end
 end)
-end
-end)
-minetest.register_chatcommand("b64", {
+core.register_chatcommand("b64", {
     description = "Say in base64",
     func = function(param)
-core.send_chat_message(core.encode_base64(param))
+        core.send_chat_message(core.encode_base64(param))
 end})
