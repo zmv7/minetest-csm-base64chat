c1512d7589d72151136c8305ecfeca0ae66e9a1a
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-12 00:37:27 +0500

    sender matching improvement

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 core.register_on_receiving_chat_message(function(message)
     message = core.strip_colors(message)
     local text
-    local sender, data = message:match("(%S+[:?>?])%s?(.+)")
+    local sender, data = message:match("<?(%S+)[:?>?]%s?(.+)")
     if data then
         text = core.decode_base64(data)
     end
     if text and sender then
         core.after(0.1,function()
-            core.display_chat_message(core.colorize('#FF0','Base64: '..sender).." "..text)
+            core.display_chat_message(core.colorize('#FF0','Base64: '..sender)..": "..text)
         end)
     end
 end)
